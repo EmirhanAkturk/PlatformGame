572243203164a74f0e3a7f4c4cf1365b29d72a9e
Emirhan Aktürk <61085526+EmirhanAkturk@users.noreply.github.com>
2020-09-06 18:54:01 +0300

    Horizontal move added

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+
+    Rigidbody2D playerRB;
+    public float moveSpeed = 1f;
+
+    bool facingRight = true;
+
+    void Awake()
+    {
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        HorizontalMove();
         
+        if(playerRB.velocity.x < 0 && facingRight){
+            //flip face
+            FlipFace();
+        }
+        else if(playerRB.velocity.x >0 && !facingRight){
+            //flip face
+            FlipFace();
+        }
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    void HorizontalMove()
+    {
+        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, playerRB.velocity.y);
+    }
+
+    void FlipFace()
+    {
+        facingRight = !facingRight;
+        Vector3 tempLocalScale = transform.localScale;
+        tempLocalScale.x *= -1;
+        transform.localScale = tempLocalScale;
     }
 }
