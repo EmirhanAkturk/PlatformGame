e03f943d2406d23870c11f9f4a776763437999cf
Emirhan Aktürk <61085526+EmirhanAkturk@users.noreply.github.com>
2020-09-10 14:41:12 +0300

    Player manager and Enemy Manager added

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public float health;
+    public float damage;
+
+    bool colliderBusy = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if( other.tag == "Player" && !colliderBusy)
+        {
+            colliderBusy = true;
+            other.GetComponent<PlayerManager>().GetDamage(damage);
+
+        }
+    }
+
+    /*
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        print("OnTriggerStay2D");
+    }
+    */
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if ( other.tag == "Player" )
+        {
+            colliderBusy = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float health;
+
+    bool dead = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GetDamage(float damage)
+    {
+        if( (health - damage) >= 0)
+        {
+            health -= damage;
+        }
+        else
+        {
+            health = 0;
+        }
+
+        AmIDead();
+    }
+
+    void AmIDead()
+    {
+        if(health <=0)
+        {
+            dead = true;
+        }
+    }
+
+}
