dc395d090882adfcc0e50ce0349a9f84299ad543
Emirhan Aktürk <61085526+EmirhanAkturk@users.noreply.github.com>
2020-09-13 18:56:55 +0300

    Game menu prepared

---
+++ Assets/Scripts/MenuManagerInGame.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManagerInGame : MonoBehaviour
+{
+    public GameObject inGameScreen, pauseScreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseButton()
+    {
+        Time.timeScale = 0;
+
+        inGameScreen.SetActive(false);
+        pauseScreen.SetActive(true);
+    }
+
+    public void PlayButton()
+    {
+        Time.timeScale = 1;
+
+        pauseScreen.SetActive(false);
+        inGameScreen.SetActive(true);
+    }
+
+    public void RePLayButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(1);
+
+    }
+
+    public void HomeButton()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
+
+}
